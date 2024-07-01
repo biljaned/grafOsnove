f52c71625cc80f7c869b956f0009b50e175eae37
Biljana  Nedic <biljana.nedic@brezasoftware.com>
2024-07-01 12:17:28 +0200

    DFS-pretraga u dubinu

--- main.cpp
+++ main.cpp
@@ -5,7 +5,14 @@ using namespace std;
 const int MAXN=1005;//maximalan broj cvorova
 vector<vector<int>> graf1(MAXN,vector<int>(0));//1. (bolji i korisceniji) nacin
 bool graf2[MAXN][MAXN];//2. nacin
-
+vector<bool> visited(MAXN,0);
+void dfs(int node){//pretraga u dubinu
+    if(visited[node]) return;
+    cout << node << " ";
+    visited[node]=true;
+    for(int u:graf1[node])
+        dfs(u);
+}
 
 int main()
 {
@@ -20,13 +27,28 @@ int main()
         graf2[a][b]=true;
         graf2[b][a]=true;
     }
-    //ispisivanje grafa
+    //ispisivanje grafa1
+    cout << "1. nacin" << endl;
     for(int i=1; i<=n; i++){
         cout << "povezani sa " << i << " su: ";
         for(int x:graf1[i])
             cout << x << " ";
         cout << endl;
     }
+    //ispisivanje grafa2
+    cout << "2. nacin" << endl;
+    for(int i=1; i<=n; i++){
+        cout << "povezani sa " << i << " su: ";
+        for(int j=1; j<=n; j++){
+            if(graf2[i][j])
+                cout << j << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    cout << "poseceni po dfs: ";
+    dfs(1);//pocinjemo pretragu iz 1. cvora po metodi deep first search
+    cout << endl;
 
 
     return 0;
